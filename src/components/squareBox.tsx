6dd7d0020d9d54bce9a3109055154738bda5ecc0
TrueJacobG <jakubgradzewicz1309@gmail.com>
2025-02-08 10:41:56 +0100

    alpha

--- src/components/squareBox.tsx
+++ src/components/squareBox.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
 
-const SquareBox = ({ title, description, link, linkText }: {title: string, description: string, link: string, linkText: string}) => {
+interface Props {
+    title: string,
+    description: string,
+    link: string,
+    linkText: string
+}
+
+const SquareBox = ({title, description, link, linkText}: Props) => {
     return (
-        <div className="bg-white p-6 rounded-lg shadow-md">w
+        <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2>{title}</h2>
             <p className="text-gray-700 mb-4">
                 {description}
             </p>
